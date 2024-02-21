bc5b1db37d4eb5374056986e1f3d807352faa79f
jurezgubic <jure.zgubi@gmail.com>
2024-02-21 15:49:09 +0000

    Adding placeholder functioncs for biperiodicity functionality

--- lib/cloudfield.py
+++ lib/cloudfield.py
@@ -6,12 +6,12 @@ from scipy.ndimage import binary_dilation
 class CloudField:
     def __init__(self, l_data,  timestep, config):
         self.timestep = timestep
-        self.clouds = self.identify_clouds(l_data, config)
+        labeled_array, self.clouds = self.identify_clouds(l_data, config)
+        self.labeled_array = self.merge_boundary_clouds(labeled_array, config)
+        self.update_clouds_from_labeled_array()
+
 
     def identify_clouds(self, l_data, config):
-        print("Starting cloud identification...")
-
-        # Create a condition array based on the threshold
         condition = l_data > config['l_condition']
 
         # Label the objects in the array
@@ -43,7 +43,44 @@ class CloudField:
                     points=points,
                     timestep=self.timestep
                 )
-
         print(f"Cloud data for {len(clouds)} objects.")
-        return clouds
 
+        return labeled_array, clouds
+
+    def merge_boundary_clouds(self, labeled_array, config):
+        edge_clouds = self.identify_edge_clouds(labeled_array)
+        merges = self.find_boundary_merges(edge_clouds, labeled_array.shape)
+        updated_array = self.update_labels_for_merges(labeled_array, merges)
+        return labeled_array
+
+    def identify_edge_clouds(self, labeled_array):
+        top_edge = set(labeled_array[0, :].flatten())
+        bottom_edge = set(labeled_array[-1, :].flatten())
+        left_edge = set(labeled_array[:, 0].flatten())
+        right_edge = set(labeled_array[:, -1].flatten())
+        return top_edge, bottom_edge, left_edge, right_edge
+
+    def find_boundary_merges(self, edge_clouds, array_shape):
+        # ---todo---. needs work! This is just a placeholder
+        # this merges anything with anything, add something that checks location
+        top_edge, bottom_edge, left_edge, right_edge = edge_clouds
+        merges = []
+        if top_edge & bottom_edge:
+            merges.append((top_edge & bottom_edge, 'y merge'))
+        if left_edge & right_edge:
+            merges.append((left_edge & right_edge, 'x merge'))
+        return merges
+
+
+    def update_labels_for_merges(self, labeled_array, merges):
+        for merge_set, direction in merges:
+            for label in merge_set:
+                # ---todo--- also needs changing
+                labeled_array[labeled_array == label] = min(merge_set)
+        return labeled_array
+
+    def update_clouds_from_labeled_array(self):
+        # ---todo---. needs work! This is just a placeholder
+        # should update self.clouds to reflect the merges. 
+        # This might involve recalculating cloud properties and ensuring cloud IDs are consistent.
+        pass
